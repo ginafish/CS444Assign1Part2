04d76bfcdbb33318c56f09372138f59809d38a70
Nawwaf Almutairi <almutnaw@os-class.engr.oregonstate.edu>
2017-05-08 19:52:54 -0700

    fixed few bugs and made sure the code compile

--- assign2scheduler/sstf-iosched.c
+++ assign2scheduler/sstf-iosched.c
@@ -1,3 +1,6 @@
+/*
+ * elevator sstf look
+ */
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
 #include <linux/bio.h>
@@ -5,25 +8,19 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
-//notes:
-//need buffer of requests
-//if scanner is going up, honors quests from low to high until reaches end and then
-//switch directions
-//if scanner is going down, honors requests from high to low until reaches end, and then
-//switch directions
-
-
-//holds queue
 struct sstf_data {
 	struct list_head queue;
-}
 
-//removes the queuelist that is next's head, and then reinitialize it, apparently?
-static void sstf_merged_requests(struct request_queue *q, struct request *rq, struct request *next) {
+	int direction;
+	sector_t head;
+};
+
+static void sstf_merged_requests(struct request_queue *q, struct request *rq, struct request *next)
+{
 	list_del_init(&next->queuelist);
 }
 
-//not sure exactly what it's doing
+
 static int sstf_dispatch(struct request_queue *q, int force) {
 	struct sstf_data *nd = q->elevator->elevator_data;
 	
@@ -37,115 +34,117 @@ static int sstf_dispatch(struct request_queue *q, int force) {
 	return 0;
 }
 
-//adds request to the correct location of the queue
-static void sstf_add_request(struct request_queue *q, struct request *rq) {
-	struct sstf_data *nd = q->elevator->elevator_data;
-	struct request *next_req, *prev_req;
-	//list_add_tail(&rq->queuelist, &nd->queue);
+static void sstf_add_request(struct request_queue *q, struct request *rq)
+{
+    struct sstf_data *nd = q->elevator->elevator_data;
+    struct request *next_req, *prev_rq;
 
-	 if (list_empty(&nd->queue)) {
-		printk("add_request to an empty list\n");
+	printk("Add called\n");
 
-		// If empty list then just add the request
+    if (list_empty(&nd->queue)) {
+		printk("Adding to an empty list\n");
+
+		// if empty list then just add the request
         list_add(&rq->queuelist, &nd->queue);
-    }else{
-    	printk("add_request to non empty list\n");
-    	
-    	// get the value for the next and prev
-    	next_req = list_entry(nd->queue.next, struct request, queuelist);
-        prev_req = list_entry(nd->queue.prev, struct request, queuelist);
+    } else {
+		printk("Adding to non empty list\n");
 
-        // go through the list until you find the right location
-        while (blk_rq_pos(rq) < blk_rq_pos(next_req)) {
+		// Get the prev and next nodes of the list
+        next_req = list_entry(nd->queue.next, struct request, queuelist);
+        prev_rq = list_entry(nd->queue.prev, struct request, queuelist);
+
+		// Go through the queue till find the right location
+        while (blk_rq_pos(rq) > blk_rq_pos(next_req)) {
             next_req = list_entry(next_req->queuelist.next, struct request, queuelist);
-            prev_req = list_entry(prev_req->queuelist.prev, struct request, queuelist);
+            prev_rq = list_entry(prev_rq->queuelist.prev, struct request, queuelist);
         }
 
-        // add the request to the list
+		// Add request to the correct location in the queue
         list_add(&rq->queuelist, &prev_rq->queuelist);
-
     }
+
 }
 
-//if the previous request in *rq is the new request? return null
-static struct request * sstf_former_request(struct request_queue *q, struct request *rq) {
+static struct request * sstf_former_request(struct request_queue *q, struct request *rq)
+{
 	struct sstf_data *nd = q->elevator->elevator_data;
-	
-	if(rq->queuelist.prev == &nd->queue) {
-		return null;
-	}
+
+	if (rq->queuelist.prev == &nd->queue)
+		return NULL;
 	return list_entry(rq->queuelist.prev, struct request, queuelist);
 }
 
-//reverse of the previous function?
-static struct request * sstf_latter_request(struct request_queue *q, struct request *rq) {
+static struct request * sstf_latter_request(struct request_queue *q, struct request *rq)
+{
 	struct sstf_data *nd = q->elevator->elevator_data;
-	if(rq->queuelist.next == &nd->queue) {
+
+	if (rq->queuelist.next == &nd->queue)
 		return NULL;
-	}
 	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-//initializes the queue
-static int sstf_init_queue(struct request_queue *q, struct elevator_type *e) {
+static int sstf_init_queue(struct request_queue *q, struct elevator_type *e)
+{
 	struct sstf_data *nd;
 	struct elevator_queue *eq;
-	
+
 	eq = elevator_alloc(q, e);
-	
-	if(!eq)
+	if (!eq)
 		return -ENOMEM;
-	
+
 	nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
-	
-	if(!nd) {
+	if (!nd) {
 		kobject_put(&eq->kobj);
 		return -ENOMEM;
 	}
+
+	nd->head = 0;
 	eq->elevator_data = nd;
-	
+
 	INIT_LIST_HEAD(&nd->queue);
-	
-	spin_lock_irq(q->queue_lock);	//locks memory from use to keep from having race conditions
+
+	spin_lock_irq(q->queue_lock);
 	q->elevator = eq;
-	spin_unlock_irq(q->queue->lock);
+	spin_unlock_irq(q->queue_lock);
 	return 0;
 }
 
-//replaces working/current queue with elevator queue?
-static void sstf_exit_queue(struct elevator_queue *e) {
+static void sstf_exit_queue(struct elevator_queue *e)
+{
 	struct sstf_data *nd = e->elevator_data;
-	
+
 	BUG_ON(!list_empty(&nd->queue));
 	kfree(nd);
 }
 
-//some kind of struct that is binding stuff in the elevator to functions in here
 static struct elevator_type elevator_sstf = {
 	.ops = {
-		.elevator_merge_req_fn		= sstf_merged_requests;
-		.elevator_dispatch_fn		= sstf_dispatch,
-		.elevator_add_req_fn		= sstf_add_request,
-		.elevator_former_req_fn		= sstf_former_request,
-		.elevator_latter_req_fn		= sstf_latter_request,
-		.elevator_init_fn			= sstf_init_queue,
-		.elevator_exit_fn			= sstf_exit_queue,
+		.elevator_merge_req_fn		 = sstf_merged_requests,
+		.elevator_dispatch_fn		 = sstf_dispatch,
+		.elevator_add_req_fn		 = sstf_add_request,
+		.elevator_former_req_fn		 = sstf_former_request,
+		.elevator_latter_req_fn		 = sstf_latter_request,
+		.elevator_init_fn		     = sstf_init_queue,
+		.elevator_exit_fn		     = sstf_exit_queue,
 	},
-	.elevator_name = "sstf",
+	.elevator_name = "look",
 	.elevator_owner = THIS_MODULE,
 };
 
-static int __init sstf_init(void) {
-	elv_register(&elevator_sstf);
+static int __init sstf_init(void)
+{
+	return elv_register(&elevator_sstf);
 }
 
-static void __exit sstf_exit(void) {
+static void __exit sstf_exit(void)
+{
 	elv_unregister(&elevator_sstf);
 }
 
 module_init(sstf_init);
 module_exit(sstf_exit);
 
+
 MODULE_AUTHOR("GinaPhipps_NawwafAlmutairi_BrandonThenell");
 MODULE_LICENSE("");
-MODULE_DESCRIPTION("sstf IO scheduler");
+MODULE_DESCRIPTION("SSTF IO scheduler");
